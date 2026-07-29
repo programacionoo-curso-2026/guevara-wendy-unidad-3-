2c320a90b1ef5e4bba7766156bfe51057b7fde20
wendy0305 <wepanoragu@uide.edu.ec>
2026-07-28 22:03:38 -0500

    Se agregó la búsqueda de un docente por correo electrónico y la visualización del resultado o del mensaje de error correspondiente

--- deber5-docente_dao/main.go
+++ deber5-docente_dao/main.go
@@ -22,4 +22,39 @@ func main() {
 		log.Fatalf("Error al crear tabla: %v", err)
 	}
 
+	// 2. INSERTAR un docente
+	docente1 := &model.Docente{
+		ID:              "D001",
+		Nombre:          "Ana García",
+		Email:           "ana.garcia@email.com",
+		Departamento:    "Informática",
+		Cargo:           "Profesora",
+		AniosAntiguedad: 5,
+	}
 
+	if err := docenteDAO.Insert(docente1); err != nil {
+		log.Printf("Error al insertar: %v", err)
+	}
+
+	// 3. BUSCAR un docente por ID
+	docente, err := docenteDAO.GetByID("D001")
+	if err != nil {
+		log.Printf("Error al buscar: %v", err)
+	} else {
+		log.Printf("Docente encontrado: %+v", docente)
+	}
+	// 4. BUSCAR por Email
+
+	docentePorEmail, err := docenteDAO.GetByEmail("carlos.ruiz@email.com")
+
+	if err != nil {
+
+		log.Printf("Error al buscar por email: %v", err)
+
+	} else {
+
+		log.Printf("Docente encontrado por email: %+v", docentePorEmail)
+
+	}
+
+}
